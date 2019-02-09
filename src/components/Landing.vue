85ac32e32c0de0537454054afd0fe71662bb3e03
AncaMilea <aem1u17@soton.ac.uk>
2019-02-09 23:08:45 +0000

    title

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,11 +2,8 @@
 
   <div class="welcome">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <div class="jumbotron">
-  <h1 class="display-4 my_style">Lorem Ipsum</h1>
-  <hr class="my-4">
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam enim odio, convallis eleifend lorem sit amet, interdum tristique lorem. Nam et risus ligula. Proin molestie metus sit amet magna aliquet pulvinar. Nullam ante risus, accumsan ultricies sagittis quis, malesuada et dui. Sed finibus at nunc sit amet vulputate. Nullam vitae dolor non ante rutrum tristique ultricies at ligula. Curabitur id velit euismod augue faucibus vulputate tristique rutrum leo. Morbi odio dui, malesuada ut quam nec, mollis maximus purus. Cras dui elit, euismod at interdum eget, feugiat nec leo. Nam sagittis luctus purus. Pellentesque pharetra lorem quis purus maximus, a consequat neque placerat. Donec ultrices est sem, id vestibulum arcu facilisis sed. Curabitur ornare massa et ipsum iaculis, non sagittis tellus scelerisque. Proin volutpat ipsum non sollicitudin posuere. Quisque ut leo lorem. Proin egestas rutrum dui vel elementum. </p>
-  
+    <div>
+    <img src="../assets/cover2.jpg" fluid-grow width=100%; height=400px; alt="Pedlar" />
 </div>
 
 <div class="container ">
